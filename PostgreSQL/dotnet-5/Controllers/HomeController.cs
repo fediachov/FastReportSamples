450f1a1c4dd7202ce1bec4a9665614ea4a211b26
Aleksandr Fediashov <fediachov@fast-report.com>
2020-12-04 09:31:11 +0300

    add net5 sample

---
+++ PostgreSQL/dotnet-5/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using core.Models;
+using FastReport;
+using FastReport.Web;
+
+using System.IO;
+
+namespace core.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new HomeModel();
+            model.WebReport = new WebReport();
+            model.WebReport.Report.Load(Path.Combine("Reports", "postgres.frx"));
+            model.WebReport.DesignerLocale = "ru";
+            model.WebReport.Width = "100%";
+            model.WebReport.Height = "800";
+            // model.WebReport.Mode = WebReportMode.Designer;
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
