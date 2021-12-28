3207d07d45af2f0bc0aa3eff74ba0a65c5db8325
Aleksandr <aleksandr@rhel8.reportinglab>
2021-12-28 12:10:08 +0300

    add RHEL example

---
+++ RHEL/frtest/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using frtest.Models;
+using FastReport;
+using FastReport.Web;
+using FastReport.Utils;
+using FastReport.Export.Pdf;
+
+namespace frtest.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        var model = new HomeModel();
+        model.WebReport = new WebReport();
+        model.WebReport.Report.Load(Path.Combine("Reports", "Text.frx"));
+        model.WebReport.Width = "100%";
+        model.WebReport.Height = "800";
+        return View(model);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
